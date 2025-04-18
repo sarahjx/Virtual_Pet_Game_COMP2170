02b703a168cce37ce854a45e6dc460d9411b9f75
Esteb4ncd <davidecruzd10@gmail.com>
2025-04-17 18:36:37 -0700

    Onboarding/ui folder  set ups

---
+++ app/onboarding/onboarding.js
@@ -0,0 +1,10 @@
+import DialogueBox from "../ui/dialogue/DialogueBox";
+
+function Onboarding() {
+    return (
+        <div>
+            <h1>Onboarding</h1>
+            <DialogueBox />
+        </div>
+    );
+}
---
+++ app/ui/dialogue/DialogueBox.js
@@ -0,0 +1,13 @@
+
+
+function DialogueBox() {
+    let dialogue= "hello";
+
+    return (
+        <div>
+            <p>${dialogue}</p>
+        </div>
+    )
+}
+
+export default DialogueBox;
